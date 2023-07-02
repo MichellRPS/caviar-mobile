3b6616b7ec9d68858699d689a4e1f14532bdc68d
Gustavo Garcia <58672038+gustavogarciaps@users.noreply.github.com>
2023-07-02 10:35:47 -0300

    Ajustando API
    Ajustando o retorno JSON do auth.php e incluindo uma documentação das rotas no index.html

--- server/auth.php
+++ server/auth.php
@@ -13,8 +13,19 @@ function autenticar($email, $senha) {
     $cliente = $statement->fetch(PDO::FETCH_ASSOC);
 
     if ($cliente) {
-        $cliente['tipo'] = 'cliente';
-        return $cliente;
+        $usuario = array(
+            'tipo' => 'cliente',
+            'id' => $cliente["id"],
+            'nome' => $cliente["nome"],
+            'sobrenome' => $cliente["sobrenome"],
+            'email' => $cliente["email"],
+            'senha' => $cliente["senha"],
+            'cpf' => $cliente["cpf"],
+            'data_nascimento' => $cliente["data_nascimento"],
+            'celular' => $cliente["celular"]
+        );
+
+        return $usuario;
     }
 
     $statement = $pdo->prepare('SELECT * FROM funcionarios WHERE email = :email AND senha = :senha');
@@ -24,8 +35,18 @@ function autenticar($email, $senha) {
     $funcionario = $statement->fetch(PDO::FETCH_ASSOC);
 
     if ($funcionario) {
-        $funcionario['tipo'] = 'funcionario';
-        return $funcionario;
+        $usuario = array(
+            'tipo' => 'funcionario',
+            'id' => $funcionario["id"],
+            'nome' => $funcionario["nome"],
+            'sobrenome' => $funcionario["sobrenome"],
+            'email' => $funcionario["email"],
+            'senha' => $funcionario["senha"],
+            'cpf' => $funcionario["cpf"],
+            'data_nascimento' => $funcionario["data_nascimento"],
+            'celular' => $funcionario["celular"]
+        );
+        return $usuario;
     }
 
     return false;
@@ -38,10 +59,10 @@ if ($route === '/auth.php/login') {
         $email = $_POST['email'];
         $senha = $_POST['senha'];
 
-        $user = autenticar($email, $senha);
+        $usuario_autenticado = autenticar($email, $senha);
 
-        if ($user) {
-            $response = array('status' => 'success', 'tipo' => $user['tipo']);
+        if ($usuario_autenticado) {
+            $response = array('status' => 'success', 'usuario' => $usuario_autenticado);
         } else {
             $response = array('status' => 'error', 'message' => 'Email ou senha inválidos');
         }
